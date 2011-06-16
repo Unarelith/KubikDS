ef4dc95ac71bd9cc6393611c401fed630632c6a9
Quentin Bazin <quent.8@hotmail.fr>
2011-06-16 15:08:45 +0200

    main.c improved.

--- source/game.cpp
+++ source/game.cpp
@@ -1,4 +1,5 @@
 #include "game.h"
+#include "efs_lib.h"
 #include "levelEditor.h"
 
 int Game::frame = 0;
@@ -10,6 +11,36 @@ Level* Game::currentLevel = 0;
 touchPosition Game::touch;
 
 Game::Game() {
+	defaultExceptionHandler();
+	
+	// Initialize random seed
+	srand(time(NULL));
+	
+	// Setup the video modes
+	videoSetMode(MODE_5_2D);
+	videoSetModeSub(MODE_0_2D);
+	
+	// Setup VRAM banks
+	vramSetBankA(VRAM_A_MAIN_SPRITE);
+	vramSetBankB(VRAM_B_MAIN_BG_0x06000000);
+	vramSetBankC(VRAM_C_SUB_BG);
+	
+	// Initialize the console
+	PrintConsole bottomScreen;
+	consoleInit(&bottomScreen, 3, BgType_Text4bpp, BgSize_T_256x256, 16, 0, false, true);
+	
+	printf("EFS loading...");
+	
+	// Initialize EFS filesystem
+	if(!EFS_Init(EFS_AND_FAT | EFS_DEFAULT_DEVICE, NULL)) {
+		printf("FATAL ERROR: Bad filesystem init");
+		while(1) { swiWaitForVBlank(); }
+	}
+	
+	printf("EFS loaded!");
+	
+	// Initialize the oam ( Sprite system )
+	oamInit(&oamMain, SpriteMapping_Bmp_1D_256, false);
 	
 	// Initialize the background
 	s_bg = bgInit(0, BgType_Text8bpp, BgSize_T_512x512, 0, 1);
--- source/main.cpp
+++ source/main.cpp
@@ -6,42 +6,10 @@
 ---------------------------------------------------------------------------------*/
 #include "main.h"
 #include "game.h"
-#include "efs_lib.h"
 
 int i;
 
 int main(void) {
-	defaultExceptionHandler();
-	
-	// Initialize random seed
-	srand(time(NULL));
-	
-	// Setup the video modes
-	videoSetMode(MODE_5_2D);
-	videoSetModeSub(MODE_0_2D);
-	
-	// Setup VRAM banks
-	vramSetBankA(VRAM_A_MAIN_SPRITE);
-	vramSetBankB(VRAM_B_MAIN_BG_0x06000000);
-	vramSetBankC(VRAM_C_SUB_BG);
-	
-	// Initialize the console
-	PrintConsole bottomScreen;
-	consoleInit(&bottomScreen, 3, BgType_Text4bpp, BgSize_T_256x256, 16, 0, false, true);
-	
-	printf("EFS loading...");
-	
-	// Initialize EFS filesystem
-	if(!EFS_Init(EFS_AND_FAT | EFS_DEFAULT_DEVICE, NULL)) {
-		printf("FATAL ERROR: Bad filesystem init");
-		while(1) { swiWaitForVBlank(); }
-	}
-	
-	printf("EFS loaded!");
-	
-	// Initialize the oam ( Sprite system )
-	oamInit(&oamMain, SpriteMapping_Bmp_1D_256, false);
-	
 	// Initialize game
 	Game* game = new Game;
 	
